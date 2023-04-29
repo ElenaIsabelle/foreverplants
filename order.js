8cb8258b2e06d3df9c5faf08cd4fbf8d9f67b090
ElenaIsabelle <125608739+ElenaIsabelle@users.noreply.github.com>
2023-04-29 11:37:45 -0400

    Add files via upload

---
+++ order.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/*
+	Elena Phillips
+	4/23
+	order.js
+	
+		DOC NOTES
+	==============================================
+   
+*/
+
+var item = [201, 432, 108, 023];
+
+var itemDescription = ["Gerber Daisies (Large), Item 201", 
+"Blue Handmade Clay Pot (3 gallon), Item 432", 
+"Geranium Seeds (20 seeds), Item 108", 
+"Fertilizer (20 lbs.), Item 023"];
+
+var itemPrice = [20.00, 128.00, 3.50, 9.50];
+
+var itemQty = [1, 1, 4, 1];
